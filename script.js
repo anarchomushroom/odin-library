5d7e24c47102c70fbb0e6769dcc01ea39241ddcf
Pádraig <prushguitar@gmail.com>
2024-01-24 16:18:41 +0000

    Render library on page load

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const container = document.querySelector(".container");
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -10,3 +12,25 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {
 
 };
+
+function render() {
+    myLibrary.forEach(book => {
+        const card = document.createElement("div")
+        card.dataset.id = myLibrary.indexOf(book);
+        container.appendChild(card);
+        
+        card.innerHTML =
+        `<h3 class="book-title">${book.title}</h3>
+        <p class="book-author">By ${book.author}</p>
+        <p class="book-pages">${book.pages} pages</p>
+        <p class="book-read">${book.read}</p>`
+    });
+}
+
+const theHobbit = new Book("The Hobbit", "JRR Tolkien", 284, "Not Read");
+const bible = new Book("The Bible", "Jesus", 666, "Read");
+
+myLibrary.push(theHobbit);
+myLibrary.push(bible);
+
+render();
